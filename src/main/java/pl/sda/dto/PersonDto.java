476f4b8ed11f6a668a5924b18e8ea97de0736d52
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-15 10:57:40 +0100

    person controller

--- src/main/java/pl/sda/dto/PersonDto.java
+++ src/main/java/pl/sda/dto/PersonDto.java
@@ -2,11 +2,13 @@ package pl.sda.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Builder
 @Data
+@NoArgsConstructor
 public class PersonDto implements Serializable {
 
     private Long id;
